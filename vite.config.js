5be72c43d99ea30452814fbb0854e992b7fc3154
Vikesh raut <Vikeshraut952@gmail.com>
2024-06-30 15:49:33 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api/v1/users': "https://backendmern-r876.onrender.com"
+      '/api/v1/users': {
+        target: 'https://backendmern-r876.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/v1\/users/, '')
+      }
     }
-  },
-  plugins: [react()]
-})
+  }
+});
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api/v1/users': "https://backendmern-r876.onrender.com"
+//     }
+//   },
+//   plugins: [react()]
+// })
