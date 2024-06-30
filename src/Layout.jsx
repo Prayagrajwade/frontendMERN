67ac9efbc10d0d55d58d2a6ef2d6b7090e83956d
Vikesh raut <Vikeshraut952@gmail.com>
2024-06-30 21:29:03 +0530

    error gone

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
-import Footer from './components/Footer/Footer'
+import Footer from './LandingPage/Footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
-import Header from './components/Header/Header'
+import Header from './LandingPage/Header/Header.jsx'
 
 function Layout() {
     return (
