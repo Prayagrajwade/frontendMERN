81e3de2df15a051def83c361722c74565a308637
Vikesh raut <Vikeshraut952@gmail.com>
2024-06-30 18:46:29 +0530

    Login UI updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import { ArrowRight } from 'lucide-react'
 const Login = ({ setIsAuthenticated }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -33,58 +33,208 @@ const Login = ({ setIsAuthenticated }) => {
     }
 
     return (
-        <div style={{ maxWidth: "500px", margin: "0 auto" }}>
-            <header style={{ color: "#224F63" }}>
-                <h1>Login</h1>
-            </header>
-            <form onSubmit={handleLogin}>
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        marginTop: "20px",
-                    }}
-                >
-                    <input
-                        type="text"
-                        placeholder="Enter Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        style={{
-                            padding: "20px",
-                            marginBottom: "10px",
-                            border: "1px solid #ccc",
-                            borderRadius: "5px",
-                            fontSize: "16px",
-                        }}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Enter Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        style={{
-                            padding: "20px",
-                            marginBottom: "10px",
-                            border: "1px solid #ccc",
-                            borderRadius: "5px",
-                            fontSize: "16px",
-                        }}
-                    />
-                    <button type="submit">Login</button>
-                    <p>
-                        {" "}
-                        Not registered?
-                        <button onClick={handleRegister}>Register</button>
-                    </p>
-                    {message && <p>{message}</p>}
+        <section>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div className="relative flex items-end px-4 pb-10 pt-60 sm:px-6 sm:pb-16 md:justify-center lg:px-8 lg:pb-24">
+                    <div className="absolute inset-0">
+                        <img
+                            className="h-full w-full rounded-md object-cover object-top"
+                            src="https://images.unsplash.com/photo-1534120247760-c44c3e4a62f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTk0fHxkZXNpZ25lcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+                            alt=""
+                        />
+                    </div>
+                    <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
+                    <div className="relative">
+                        <div className="w-full max-w-xl xl:mx-auto xl:w-full xl:max-w-xl xl:pr-24">
+                            <h3 className="text-4xl font-bold text-white">
+                                Now you dont have to rely on your designer to create a new page
+                            </h3>
+                            <ul className="mt-10 grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2">
+                                <li className="flex items-center space-x-3">
+                                    <div className="inline-flex h-5 w-5 flex-shrink-0 items-center justify-center rounded-full bg-blue-500">
+                                        <svg
+                                            className="h-3.5 w-3.5 text-white"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                    <span className="text-lg font-medium text-white"> Commercial License </span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <div className="inline-flex h-5 w-5 flex-shrink-0 items-center justify-center rounded-full bg-blue-500">
+                                        <svg
+                                            className="h-3.5 w-3.5 text-white"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                    <span className="text-lg font-medium text-white"> Unlimited Exports </span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <div className="inline-flex h-5 w-5 flex-shrink-0 items-center justify-center rounded-full bg-blue-500">
+                                        <svg
+                                            className="h-3.5 w-3.5 text-white"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                    <span className="text-lg font-medium text-white"> 120+ Coded Blocks </span>
+                                </li>
+                                <li className="flex items-center space-x-3">
+                                    <div className="inline-flex h-5 w-5 flex-shrink-0 items-center justify-center rounded-full bg-blue-500">
+                                        <svg
+                                            className="h-3.5 w-3.5 text-white"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                clipRule="evenodd"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                    <span className="text-lg font-medium text-white"> Design Files Included </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </form>
-        </div>
-    );
-};
+                <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+                    <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign in</h2>
+                        <p className="mt-2 text-sm text-gray-600">
 
+                            {" "}
+                            Not registered?
+                            <button onClick={handleRegister}>Register</button>
+                        </p>
+
+                        <form onSubmit={handleLogin} className="mt-8">
+                            <div className="space-y-5">
+                                <div>
+                                    <label htmlFor="" className="text-base font-medium text-gray-900">
+                                        {' '}
+                                        Email address{' '}
+                                    </label>
+
+                                    <div className="mt-2">
+                                        <input
+                                            type="text"
+                                            placeholder="Enter Email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                                        ></input>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div className="mt-2">
+                                        <input
+                                            type="password"
+                                            placeholder="Enter Password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                                        ></input>
+                                    </div>
+
+                                </div>
+
+                                <div>
+                                    <button
+                                        type="submit"
+                                        className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                                    >
+                                        Get started <ArrowRight className="ml-2" size={16} />
+                                    </button>
+                                </div>
+                                {message && <p>{message}</p>}
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
 export default Login;
+
+
+// <div style={{ maxWidth: "500px", margin: "0 auto" }}>
+//             <header style={{ color: "#224F63" }}>
+//                 <h1>Login</h1>
+//             </header>
+//             <form onSubmit={handleLogin}>
+//                 <div
+//                     style={{
+//                         display: "flex",
+//                         flexDirection: "column",
+//                         marginTop: "20px",
+//                     }}
+//                 >
+//                     <input
+//                         type="text"
+//                         placeholder="Enter Email"
+//                         value={email}
+//                         onChange={(e) => setEmail(e.target.value)}
+//                         style={{
+//                             padding: "20px",
+//                             marginBottom: "10px",
+//                             border: "1px solid #ccc",
+//                             borderRadius: "5px",
+//                             fontSize: "16px",
+//                         }}
+//                     />
+//                     <input
+//                         type="password"
+//                         placeholder="Enter Password"
+//                         value={password}
+//                         onChange={(e) => setPassword(e.target.value)}
+//                         style={{
+//                             padding: "20px",
+//                             marginBottom: "10px",
+//                             border: "1px solid #ccc",
+//                             borderRadius: "5px",
+//                             fontSize: "16px",
+//                         }}
+//                     />
+//                     <button type="submit">Login</button>
+//                     <p>
+//                         {" "}
+//                         Not registered?
+//                         <button onClick={handleRegister}>Register</button>
+//                     </p>
+//                     {message && <p>{message}</p>}
+//                 </div>
+//             </form>
+//         </div>
+
+
+
 
 // import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
