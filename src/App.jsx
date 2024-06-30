66201dc156fa07a67bfa7c5cd65aee99dacc1fdb
Vikesh raut <Vikeshraut952@gmail.com>
2024-06-30 22:31:01 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Home from './LandingPage/Home/Home.jsx';
 import About from './LandingPage/About/About.jsx';
 import Contact from './LandingPage/Contact/Contact.jsx';
 import User from './LandingPage/User/User.jsx';
-import Homepage from './components/Homepage.jsx';
+// import Homepage from './components/Homepage.jsx';
 // import Github, { githubInfoLoader } from './LandingPage/Github/Github.jsx';
 
 const App = () => {
@@ -33,7 +33,6 @@ const App = () => {
           <Route path='forgetpass' element={<ForgetPassword />} />
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
-          <Route path='home' element={<Homepage />} />
           <Route path='user/:userid' element={<User />} />
           <Route path='dashboard' element={isAuthenticated ? <Dashboard setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />} />
         </Route>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,24 +1,24 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-const Homepage = ({ isAuthenticated }) => {
-    return (
-        <div>
-            <h1>Welcome to our app</h1>
-            <nav>
-                {isAuthenticated ? (
-                    <div>
-                        <Link to="/dashboard">
-                            Go to Dashboard
-                        </Link>
-                    </div>
-                ) : (
-                    <div>
-                        <Link to="/">Layout</Link>
-                    </div>
-                )}
-            </nav>
-        </div>
-    );
-};
+// const Homepage = ({ isAuthenticated }) => {
+//     return (
+//         <div>
+//             <h1>Welcome to our app</h1>
+//             <nav>
+//                 {isAuthenticated ? (
+//                     <div>
+//                         <Link to="/dashboard">
+//                             Go to Dashboard
+//                         </Link>
+//                     </div>
+//                 ) : (
+//                     <div>
+//                         <Link to="/">Layout</Link>
+//                     </div>
+//                 )}
+//             </nav>
+//         </div>
+//     );
+// };
 
-export default Homepage;
+// export default Homepage;
