263204fd6753ad04ef64f7f0d31ba6ecb305b93b
Vikesh raut <Vikeshraut952@gmail.com>
2024-07-02 14:12:15 +0530

    test

--- src/components/CurrentUser.jsx
+++ src/components/CurrentUser.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const CurrentUser = async () => {
-    const response = await axios.get("https://backendmern-r876.onrender.com/api/v1/users/current", {
+    const response = await axios.get("https://backendmern-r876.onrender.com/api/v1/users/current-user", {
         withCredentials: true,
         headers: {
             'Content-Type': 'application/json',
