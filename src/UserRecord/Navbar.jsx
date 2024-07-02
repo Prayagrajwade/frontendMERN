cec84f6e440013747d801c335d50e243b6a77fea
Vikesh raut <Vikeshraut952@gmail.com>
2024-07-02 19:04:16 +0530

    update

--- src/UserRecord/Navbar.jsx
+++ src/UserRecord/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 
-export default function Navbar({setIsAuthenticated}) {
+export default function NavBar({setIsAuthenticated}) {
     return (
         <div>
             <nav className="flex justify-between items-center mb-6">
