339ae68e5016c25e52f35eb142eb5cad896d793d
Vikesh raut <Vikeshraut952@gmail.com>
2024-07-02 23:29:00 +0530

    todo

--- src/components/Dasboard.jsx
+++ src/components/Dasboard.jsx
@@ -1,31 +1,12 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
+import React from 'react'
+import Todo from './Todo'
 
-const Dashboard = () => {
-    const [users, setUsers] = useState([]); // State to store our users
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await axios.get('https://backendmern-r876.onrender.com/api/v1/users/current-user'); // Fetch users from our API
-                setUsers(response.data); // Update state with fetched users
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        fetchUsers();
-    }, []);
-    return (
-        <div>
-            <div>Get current user</div>
-            <ul>
-                {users.map(user => (
-                    <li key={user._id}>{user.name}</li> // Display each user's name
-                ))}
-            </ul>
-        </div>
-    )
+function Dasboard() {
+  return (
+    <div>
+        <Todo/>
+    </div>
+  )
 }
 
-export default Dashboard;
+export default Dasboard
