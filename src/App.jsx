05b44bbbba595d540e1ed5f0381f91f5c6c07f87
Vikesh raut <Vikeshraut952@gmail.com>
2024-07-02 19:09:23 +0530

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ const App = () => {
 
   return (
     <BrowserRouter>
+    <NavBar/>
       <Routes>
         <Route path="/" element={<PublicRoute element={<Layout />} />}>
           <Route path="" element={<Home />} />
@@ -51,7 +52,6 @@ const App = () => {
         <Route path="profile" element={<ProtectedRoute element={<UserProfile setIsAuthenticated={setIsAuthenticated}/>} />} />
         <Route path="record" element={<ProtectedRoute element={<Record setIsAuthenticated={setIsAuthenticated} />}/>} />
         <Route path="recordlist" element={<ProtectedRoute element={<RecordList setIsAuthenticated={setIsAuthenticated} />}/>} />
-        <Route path="navbar" element={<ProtectedRoute element={<NavBar setIsAuthenticated={setIsAuthenticated}/>}/>} />
       </Routes>
     </BrowserRouter>
   );
